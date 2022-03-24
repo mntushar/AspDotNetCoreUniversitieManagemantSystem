e3350b8c9e49c6a3d36f44a04f43b575c2d297d5
M N Tushar <mntushar25@gmail.com>
2022-03-24 12:46:05 +0600

    Create base repositorie and inteface

---
+++ Repositories/BaseRepositorie/BaseRepositorie.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.DbContexts;
+using Repositories.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repositories.BaseRepositorie
+{
+    public class BaseRepositorie<T> : IBaseRepositorie<T> where T : class
+    {
+        private UniversityDbContext _db;
+        private DbSet<T> Table
+        {
+            get { return _db.Set<T>(); }
+        }
+
+        public BaseRepositorie()
+        {
+            _db = new UniversityDbContext();
+        }
+
+        public virtual bool Add(T entity)
+        {
+            Table.Add(entity);
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual bool Update(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual bool Remove(T entity)
+        {
+            Table.Remove(entity);
+            return _db.SaveChanges() > 0;
+        }
+
+        public virtual T Get(int? id)
+        {
+            return Table.Find(id);
+        }
+
+        public virtual ICollection<T> GetAll()
+        {
+            return Table.ToList();
+        }
+    }
+}
---
+++ Repositories/Contacts/IBaseRepositorie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories.Contacts
+{
+    public interface IBaseRepositorie<T> where T : class
+    {
+        public bool Add(T entity);
+        public bool Update(T entity);
+        public bool Remove(T entity);
+        public T Get(int? id);
+        public ICollection<T> GetAll();
+    }
+}
