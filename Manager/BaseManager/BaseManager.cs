091728f122774aaca2e8c8655a5bebc43e3cc2c7
M N Tushar <mntushar25@gmail.com>
2022-03-24 12:46:42 +0600

    create base manager and interface

---
+++ Manager/BaseManager/BaseManager.cs
@@ -0,0 +1,38 @@
+﻿using Manager.Contacts;
+using Repositories.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.BaseManager
+{
+    public abstract class BaseManager<T> : IBaseManager<T> where T : class
+    {
+        private IBaseRepositorie<T> _baseRepositorie;
+
+        public BaseManager(IBaseRepositorie<T> baseRepositorie)
+        {
+            _baseRepositorie = baseRepositorie;
+        }
+
+        public bool Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Get(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Manager/Contacts/IBaseManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Contacts
+{
+    interface IBaseManager<T> where T : class
+    {
+        public bool Add(T entity);
+        public bool Update(T entity);
+        public bool Remove(T entity);
+        public bool Get(T entity);
+    }
+}
