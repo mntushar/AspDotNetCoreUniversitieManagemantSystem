ae3a4daa681e223a06e2fd270376ba39b21313ee
M N Tushar <mntushar25@gmail.com>
2022-03-26 14:07:02 +0600

    create base manager interface

--- Manager/Contacts/IBaseManager.cs
+++ Manager/Contacts/IBaseManager.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace Manager.Contacts
 {
-    interface IBaseManager<T> where T : class
+    public interface IBaseManager<T> where T : class
     {
         public bool Add(T entity);
         public bool Update(T entity);
         public bool Remove(T entity);
-        public bool Get(T entity);
+        public T Get(int? id);
+        public ICollection<T> GetAll();
     }
 }
