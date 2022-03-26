bfd8e5906a328e4a958b2d914c42323657f1150d
M N Tushar <mntushar25@gmail.com>
2022-03-26 14:10:38 +0600

    create department repositore and interface

---
+++ Repositories/DepartmentRepositorie.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using Repositories.BaseRepositorie;
+using Repositories.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    public class DepartmentRepositorie : BaseRepositorie<DepartmentModel>, IDepartmentRepositorie
+    {
+    }
+}
