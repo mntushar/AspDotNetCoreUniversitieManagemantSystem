10e851fbc9739292fc203d15ebe31331cccdd25c
M N Tushar <mntushar25@gmail.com>
2022-03-26 14:10:05 +0600

    create department manage and interface

---
+++ Manager/DepartmentManager.cs
@@ -0,0 +1,22 @@
+﻿using Manager.BaseManager;
+using Manager.Contacts;
+using Models;
+using Repositories;
+using Repositories.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class DepartmentManager : BaseManager<DepartmentModel>, IDepartmentManager
+    {
+        private IDepartmentRepositorie _departmentRepositorie;
+
+        public DepartmentManager(IDepartmentRepositorie departmentRepositorie):base(departmentRepositorie)
+        {
+            _departmentRepositorie = departmentRepositorie;
+        }
+    }
+}
