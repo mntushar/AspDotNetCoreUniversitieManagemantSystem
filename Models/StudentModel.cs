ef8581564c8a77c02a58fc470d49cdbe5cfe2d62
M N Tushar <mntushar25@gmail.com>
2022-03-27 13:35:50 +0600

    create student model

--- Models/StudentModel.cs
+++ Models/StudentModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Models
 {
     public class StudentModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required, Display(Name = "Department Name")]
+        [ForeignKey("Department")]
+        public int DeptId { get; set; }
+
+
+        //navigration proprty
+        public virtual DepartmentModel Department { get; set; }
+        //public virtual StudentRegistrationModels StudentRegistration { get; set; }
     }
 }
