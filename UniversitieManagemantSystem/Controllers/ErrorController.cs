bf361b6b57fffdd7c4829631384541b976ebcba6
M N Tushar <mntushar25@gmail.com>
2022-03-27 13:34:24 +0600

    create error class

---
+++ UniversitieManagemantSystem/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversitieManagemantSystem.Controllers
+{
+    public class ErrorController
+    {
+        public string DataSaveCustomError()
+        {
+            return "Please enter correct information..";
+        }
+    }
+}
