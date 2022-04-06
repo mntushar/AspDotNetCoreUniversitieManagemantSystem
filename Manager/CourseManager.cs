abd6e039a2a1a93af46371daa119283ea20d8ac9
M N Tushar <mntushar25@gmail.com>
2022-04-06 15:16:36 +0600

    create course manage and repositore

---
+++ Manager/CourseManager.cs
@@ -0,0 +1,20 @@
+﻿using Manager.BaseManager;
+using Manager.Contacts;
+using Models;
+using Repositories.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager
+{
+    public class CourseManager : BaseManager<CourseModel>, ICourseManager
+    {
+        private ICourseRepositorie _courseRepositore;
+
+        public CourseManager(ICourseRepositorie courseRepositore) : base(courseRepositore)
+        {
+            _courseRepositore = courseRepositore;
+        }
+    }
+}
