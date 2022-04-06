83c50cb33fa46705064c22af85c5cedd8716187d
M N Tushar <mntushar25@gmail.com>
2022-04-06 17:12:50 +0600

    create studetn regristration repositore

---
+++ Manager/Contacts/IStudentRegristrationManager.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager.Contacts
+{
+    public interface IStudentRegristrationManager : IBaseManager<StudentRegistrationModel>
+    {
+    }
+}
---
+++ Manager/StudentRegristrationManager.cs
@@ -0,0 +1,20 @@
+﻿using Manager.BaseManager;
+using Manager.Contacts;
+using Models;
+using Repositories.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager
+{
+    public class StudentRegristrationManager : BaseManager<StudentRegistrationModel>, IStudentRegristrationManager
+    {
+        private IStudentRegristration _baseRepositorie;
+
+        public StudentRegristrationManager(IStudentRegristration baseRepositorie) : base(baseRepositorie)
+        {
+            _baseRepositorie = baseRepositorie;
+        }
+    }
+}
