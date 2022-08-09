48aa76673339d129b4d8500c207afe4e1b9b3769
M N Tushar <45463161+mntushar@users.noreply.github.com>
2022-08-09 09:46:08 +0600

    change interface declare  in base repository

--- Repositories/Contacts/IBaseRepositorie.cs
+++ Repositories/Contacts/IBaseRepositorie.cs
@@ -6,10 +6,10 @@ namespace Repositories.Contacts
 {
     public interface IBaseRepositorie<T> where T : class
     {
-        public bool Add(T entity);
-        public bool Update(T entity);
-        public bool Remove(T entity);
-        public T Get(int? id);
-        public ICollection<T> GetAll();
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Remove(T entity);
+        T Get(int? id);
+        ICollection<T> GetAll();
     }
 }
